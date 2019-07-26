d866445ee73fbb3c216ade95e41713d0332b31f0
AAAatrox <2104934038@qq.com>
2019-07-26 11:17:06 +0800

    git

---
+++ my_update.sh
@@ -0,0 +1,15 @@
+if [ "$1"x = x ]
+then
+	echo "\033[1;31mNeed message\033[0m"
+else
+	echo "\033[1;32mAdding...\033[0m"
+	git commit -m $1
+	echo "\033[1;33mPushing...\033[0m"
+	if [ "$2"x != x ]
+	then
+		hexo cl && hexo g && hexo d
+		git push origin master
+	else
+		echo "\033[1;34mAbort\033[0m"
+	fi
+fi
